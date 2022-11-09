af281850866c39bcdd35b516e26d7cbbb82bc236
zero.qn <zero.qn@outlook.com>
2022-11-09 19:02:06 +0800

    fix

--- packages/api-server/opentelemetry.js
+++ packages/api-server/opentelemetry.js
@@ -2,39 +2,38 @@
 
 const process = require('process');
 const opentelemetry = require('@opentelemetry/sdk-node');
+const jaeger = require("@opentelemetry/exporter-jaeger")
+const instrumentations = require('@opentelemetry/auto-instrumentations-node');
 
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+// TODO: We can remove this after upgrade sdk-node to 0.34
+// Reference: https://github.com/open-telemetry/opentelemetry-js/pull/3388
+const jaegerExporter = new jaeger.JaegerExporter();
 
-const provider = new NodeTracerProvider()
-
-// Configuration through env:
+// Configuration (sdk 0.34 or later):
 // OTEL_TRACES_EXPORTER
+// OTEL_PROPAGATORS
 // OTEL_EXPORTER_OTLP_PROTOCOL
 // OTEL_EXPORTER_OTLP_TRACES_PROTOCOL
 // OTEL_EXPORTER_OTLP_METRICS_PROTOCOL
+// OTEL_RESOURCE_ATTRIBUTES
 //
 // For jaeger example:
 // OTEL_TRACES_EXPORTER: jaeger
+// OTEL_PROPAGATORS: jaeger
 // OTEL_EXPORTER_OTLP_PROTOCOL: grpc (default is http/protobuf)
 // OTEL_EXPORTER_JAEGER_ENDPOINT: http://jaeger:14250
+// OTEL_RESOURCE_ATTRIBUTES: service.name=web3-readonly1
 //
 // Reference: https://github.com/open-telemetry/opentelemetry-js/blob/main/experimental/packages/opentelemetry-sdk-node/README.md
 const sdk = new opentelemetry.NodeSDK({
+    traceExporter: jaegerExporter,
     instrumentations: [
-        getNodeAutoInstrumentations({
+        instrumentations.getNodeAutoInstrumentations({
             '@opentelemetry/instrumentation-redis': {},
             '@opentelemetry/instrumentation-http': {},
             '@opentelemetry/instrumentation-express': {},
-            '@opentelemetry/instrumentation-winston': {
-                // Add trace_id, span_id and trace_flags to log entry
-                // Also add 'service.name' from OTEL_RESOURCE_ATTRIBUTES env
-                //
-                // Example: OTEL_RESOURCE_ATTRIBUTES: service.name=web3-readonly1
-                logHook: (_span, _record) => {
-                    record['resource.service.name'] = provider.resource.attributes['service.name']
-                }
-            }
+            // Add trace_id, span_id and trace_flags to log entry
+            '@opentelemetry/instrumentation-winston': {},
         })
     ]
 });
